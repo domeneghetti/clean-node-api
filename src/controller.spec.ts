18ab81276b395fa23969c7ac23748cea379bf87c
Fernando Domeneghetti <f.domeneghetti@totvs.com.br>
2021-08-04 21:29:02 -0300

    feat: ensure SignUpController returns 400 if no name is provided

--- src/controller.spec.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-undef */
-// eslint-disable-next-line no-undef
-describe('first test', () => {
-  test('teste one', () => {
-    expect(1).toBe(1)
-  })
-})
---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+
+/* eslint-disable no-undef */
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordconfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
